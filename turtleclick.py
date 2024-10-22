ecfba740db74d26b919f9a811250356a2aa29b4a
Vijayesh314 <145930677+Vijayesh314@users.noreply.github.com>
2024-10-22 15:23:48 -0700

    Update turtleclick.py

--- turtleclick.py
+++ turtleclick.py
@@ -8,6 +8,7 @@ import random
 torterracolor = "Green"
 torterrasize = 3
 torterrashape = "turtle"
+global score
 
 #-----initialize turtle-----
 torterra = t.Turtle()
@@ -16,11 +17,18 @@ torterra.color(torterracolor)
 torterra.shapesize(torterrasize)
 torterra.speed(0)
 
+timer = t.Turtle()
+timer.fillcolor("white")
+
+scorer = t.Turtle()
+scorer.fillcolor("white")
+
 #-----game functions--------
 def torterraClicked (x,y):
     torterra.write("You clicked this", font=("Caliri", 15, "normal"))
     torterra.penup()
     torterra.goto(random.randrange(-400, 400), random.randrange(-300, 300))
+    score += 10
 torterra.onclick(torterraClicked)
 
 #-----events----------------
