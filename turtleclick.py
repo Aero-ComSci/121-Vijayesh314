7e90e9551acfe83dcdaa91a130b52143cf7e6ccf
Vijayesh314 <145930677+Vijayesh314@users.noreply.github.com>
2024-10-24 14:33:43 -0700

    Update turtleclick.py

--- turtleclick.py
+++ turtleclick.py
@@ -3,12 +3,15 @@
 import turtle as t
 import time
 import random
+from tkinter import Tk
 
 #-----game configuration----
 torterracolor = "Green"
+global torterrasize
 torterrasize = 3
 torterrashape = "turtle"
 global score
+score = 0
 
 #-----initialize turtle-----
 torterra = t.Turtle()
@@ -17,20 +20,37 @@ torterra.color(torterracolor)
 torterra.shapesize(torterrasize)
 torterra.speed(0)
 
-timer = t.Turtle()
-timer.fillcolor("white")
+scoreboard = t.Turtle()
+scoreboard.shapesize(0.00001)
+scoreboard.teleport(-350,300)
 
-scorer = t.Turtle()
-scorer.fillcolor("white")
+timer = t.Turtle()
+timer.hideturtle()
+timer.teleport(350,300)
 
 #-----game functions--------
-def torterraClicked (x,y):
-    torterra.write("You clicked this", font=("Caliri", 15, "normal"))
+score = 10
+def update_score():
+    global score
+    scoreboard.clear()
+    score += 10
+    scoreboard.write(f"Score: {score}", font=("Calibri", 15, "normal"))
+
+def time_up():
+    global game_over
+    game_over = True
+
+def torterraClicked(x, y):
+    global torterrasize
     torterra.penup()
     torterra.goto(random.randrange(-400, 400), random.randrange(-300, 300))
-    score += 10
 torterra.onclick(torterraClicked)
 
+if game_over:
+    timer.write("Game Over", font=("Calibri", 15, "normal"))
+
+clock.schedule(time_up, 60)
 #-----events----------------
 wn = t.Screen()
+wn.bgcolor("light gray")
 wn.mainloop()
